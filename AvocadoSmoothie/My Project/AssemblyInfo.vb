892b7ca57acbef2ea3a14c7d7c2bf13f54549575
Charlie Jaewoong Mun <happybono@outlook.com>
2025-09-28 01:32:47 +0900

    Add files via upload

--- AvocadoSmoothie/My Project/AssemblyInfo.vb
+++ AvocadoSmoothie/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("4.6.6.0")>
-<Assembly: AssemblyFileVersion("4.6.6.0")>
+<Assembly: AssemblyVersion("4.7.0.0")>
+<Assembly: AssemblyFileVersion("4.7.0.0")>
